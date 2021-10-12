a8cb72d0e25c52da6cf617c69a0c555822eb8b50
Maimonides-CS <91688173+Maimonides-CS@users.noreply.github.com>
2021-10-12 08:35:52 -0500

    Create Array1.java

---
+++ Array1.java
@@ -0,0 +1,10 @@
+public class Array1 { //class definition ,"public" means other classes have access to this class
+  public static void main(String[] args) { //method definition
+    String[] cars = {"Volvo", "BMW", "Ford", "Mazda"};
+    int[] myNum = {10, 20, 30, 40};
+    for (int i=0; i< myNum.length ; i++) {
+      System.out.println(myNum[i]);
+    }
+
+  }
+}
