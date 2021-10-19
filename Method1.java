271637a56515a447d4ce4e87fbf44870e762f192
Maimonides-CS <91688173+Maimonides-CS@users.noreply.github.com>
2021-10-19 08:02:25 -0500

    Create Method1.java

---
+++ Method1.java
@@ -0,0 +1,9 @@
+public class Method1 { //class definition ,"public" means other classes have access to this class
+      static void myMethod() {
+        System.out.println("I just got executed!");
+      }
+
+public static void main(String[] args) { //method definition
+          myMethod();
+        }
+     }
