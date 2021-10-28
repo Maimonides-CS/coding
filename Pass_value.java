ac507499b60c906e5cef8810778bf4d71b31355a
Maimonides-CS <91688173+Maimonides-CS@users.noreply.github.com>
2021-10-28 08:50:38 -0500

    Create Pass_value.java

---
+++ Pass_value.java
@@ -0,0 +1,16 @@
+public class Pass_value {
+  public static void main(String[] args) {
+    int[] a = {1, 2, 3, 4};
+    int j=99;    
+    print(a, j);
+    for (int i=0; i<a.length; i++) {
+      System.out.println(a[i]);
+    }
+    System.out.println("j: "+ j);
+  }
+
+   public static void print(int [] b, int j) {
+     b[0] = 3;
+     j = 90;
+   }
+}
