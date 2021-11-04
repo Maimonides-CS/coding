c2b8107fe9cda9c15e1a6f47b4adf66e935f7147
Maimonides-CS <91688173+Maimonides-CS@users.noreply.github.com>
2021-11-04 08:55:22 -0500

    Create Final_int.java

---
+++ Final_int.java
@@ -0,0 +1,14 @@
+public class Final_int {
+  int x = 5;
+  final int y = 4;
+  public static void main(String[] args) {
+        Hello1 myObj1 = new Hello1();
+        Hello1 myObj2 = new Hello1();
+        myObj1.x = 40;
+        myObj1.y = 10;
+        System.out.println(myObj1.x);
+        System.out.println(myObj2.x);
+      }
+}
+    
+ 
