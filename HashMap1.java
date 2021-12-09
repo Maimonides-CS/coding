2b17dfe42107c8bdea7285852b32249d9b0b9f9f
Maimonides-CS <91688173+Maimonides-CS@users.noreply.github.com>
2021-12-09 09:13:24 -0600

    Create HashMap1.java

---
+++ HashMap1.java
@@ -0,0 +1,17 @@
+// Import the HashMap class
+import java.util.HashMap;
+
+public class HashMap1 {
+  public static void main(String[] args) {
+    // Create a HashMap object called capitalCities
+    HashMap<String, String> capitalCities = new HashMap<String, String>();
+
+    // Add keys and values (Country, City)
+    capitalCities.put("England", "London");
+    capitalCities.put("Germany", "Berlin");
+    capitalCities.put("Norway", "Oslo");
+    capitalCities.put("USA", "Washington DC");
+    System.out.println(capitalCities);
+    System.out.println("Get a value from the HashMap" + capitalCities.get("England"));
+  }
+}
